4745817c0b5a2b2432d6b62273685a4a9b8eb001
Csilla Bukki <csillabukki@Csillas-MacBook-Air.local>
2024-06-26 20:38:13 -0700

    path /dogs works

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import NavBar from "./NavBar";
+import DogList from "./DogList";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import "./App.css";
+
+function App({ dogs }) {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        {/*<h1>Good luck!</h1>*/}
+        <NavBar dogs={dogs} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<h1>Go to /dogs or click on Home</h1>}
+          />
+          <Route path="/dogs" element={<DogList dogs={dogs} />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+App.defaultProps = {
+  dogs: [
+    {
+      name: "Whiskey",
+      age: 5,
+      src: "https://images.unsplash.com/photo-1422565096762-bdb997a56a84?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      facts: [
+        "Whiskey loves eating popcorn.",
+        "Whiskey is a terrible guard dog.",
+        "Whiskey wants to cuddle with you!",
+      ],
+    },
+    {
+      name: "Duke",
+      age: 3,
+      src: "https://images.unsplash.com/photo-1534361960057-19889db9621e?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      facts: [
+        "Duke believes that ball is life.",
+        "Duke likes snow.",
+        "Duke enjoys pawing other dogs.",
+      ],
+    },
+    {
+      name: "Perry",
+      age: 4,
+      src: "https://plus.unsplash.com/premium_photo-1717857017406-12c9274ff741?q=80&w=2944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      facts: [
+        "Perry loves all humans.",
+        "Perry demolishes all snacks.",
+        "Perry hates the rain.",
+      ],
+    },
+    {
+      name: "Tubby",
+      age: 4,
+      src: "https://images.unsplash.com/photo-1587300003388-59208cc962cb?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      facts: [
+        "Tubby is really stupid.",
+        "Tubby does not like walks.",
+        "Angelina used to hate Tubby, but claims not to anymore.",
+      ],
+    },
+  ],
+};
+export default App;
---
+++ src/DogList.js
@@ -0,0 +1,17 @@
+import "./DogList.css";
+const DogList = ({ dogs }) => {
+  return (
+    <div className="DogList">
+      <div className="DogList-dogs">
+        {dogs.map((dog) => (
+          <div className="DogList-dog" key={dog.name}>
+            <h3>{dog.name}</h3>
+            <img src={dog.src} alt={dog.name} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DogList;
